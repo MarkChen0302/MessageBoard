483b408e0772de39c48cf100ca77445ac1d4db0e
MarkChen0302 <102308808+MarkChen0302@users.noreply.github.com>
2022-04-01 14:32:46 +0800

    Add files via upload

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-import this
+from sqlalchemy import true
 import config
-from operator import methodcaller
 from flask import Flask, redirect, render_template, request, url_for
 from datetime import datetime
+from flask_login import LOGIN_MESSAGE, LoginManager,UserMixin,login_user,current_user,login_required,logout_user
+import os
+class User(UserMixin):
+    isAdmin=true
+    pass
 
 class WebMessage:
     id=0
@@ -14,29 +18,57 @@ class WebMessage:
         self.content=content
         self.id=id
         self.createTime=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+baseDir=os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config.from_object(config)
-user={'mark':'123'}
+app.secret_key='123'
+
+loginManager=LoginManager()
+# loginManager=LoginManager(app)
+loginManager.login_view='login'
+loginManager.login_message_category='info'
+loginManager.login_message='Access denied'
+loginManager.init_app(app)
+
+users={'mark':{'password':'123'}}
 MessageList=[WebMessage('123','321',0)]
+
+def quert_user(user_id):
+    for user in users.keys():
+        if user_id == user:
+            return user
+@loginManager.user_loader
+def load_user(user_id):
+    if quert_user(user_id) is not None:
+        curr_user=User()
+        curr_user.id=user_id
+        return curr_user
+
 @app.route('/Home/',methods=['GET'])
 def home():
     if 'username' in request.args:
         username=request.args['username']
-        return render_template('Home.html',username=username,posts=MessageList,isAdmin=True)
+        return render_template('Home.html',posts=MessageList,isAdmin=True)
     return render_template('Home.html',posts=MessageList,isAdmin=False)
 
 @app.route('/logout',methods=['GET'])
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for('home'))
+    
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         name=request.form.get('username')
         password=request.form.get('password')
-        if name in user:
-            if user[name] == password:
-                result=name
-        return redirect(url_for('home',username=result))
+        user= quert_user(name)
+        if user is not None and users[name]['password'] == password:
+            curr_user=User()
+            curr_user.id=name
+            login_user(curr_user)
+        return redirect(url_for('home'))
     else:
         return render_template('login.html')
 
